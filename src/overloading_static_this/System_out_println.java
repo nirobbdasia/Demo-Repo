9b079bb9c54a1b26b6cc5892fcad939aae5200bf
alamin khan <alamin_khan@apple.com>
2019-11-22 14:48:07 -0600

    added

---
+++ src/overloading_static_this/System_out_println.java
@@ -0,0 +1,28 @@
+package overloading_static_this;
+
+public class System_out_println {
+
+	public static void main(String[] args) {
+		/*
+		clss test
+		{
+		static string s ="welcom";
+		}
+		
+		test.s.lenght (); ---->7
+		
+		
+		class system 
+		{
+		static printStream out;//out i sa vaiable 
+		}
+		
+		system.out.println("testing);
+		 * 
+		 */
+
+		
+		
+	}
+
+}
